aa4fadf96da6efeec43ce93d06dc33f18d064d2e
Abel-Girma-WebIt <gedamu0918@gmail.com>
2024-06-30 20:29:46 -0400

    debuging get acces token

--- frontend/src/Compo/PrivateRoute.js
+++ frontend/src/Compo/PrivateRoute.js
@@ -1,21 +1,14 @@
-import {Outlet , Navigate} from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
-const PrivateRoute =()=>{
-
-
+const PrivateRoute = () => {
     let access = Cookies.get('access_token');
     let refresh = Cookies.get('refresh_token');
 
     console.log('Access Token:', access); // Debugging statement
     console.log('Refresh Token:', refresh); // Debugging statement
 
-
-
-    return (
-
-        refresh?
-<Outlet/> : <Navigate to='user/login' replace />   )
-}
+    return refresh ? <Outlet /> : <Navigate to='/user/login'/>;
+};
 
 export default PrivateRoute;

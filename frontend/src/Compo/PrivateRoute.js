d69633403e968ec4076598569451620a8755d4c3
Abel-Girma-tech <abel091881@gmail.com>
2024-07-01 09:23:57 -0400

    Update PrivateRoute.js
    Modifying private route in GIt iwebsite

--- frontend/src/Compo/PrivateRoute.js
+++ frontend/src/Compo/PrivateRoute.js
@@ -2,11 +2,11 @@ import { Outlet, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const PrivateRoute = () => {
-    let access = Cookies.get('access_token');
-    let refresh = Cookies.get('refresh_token');
+    // let access = Cookies.get('access_token');
+    let refresh =true;
 
-    console.log('Access Token:', access); // Debugging statement
-    console.log('Refresh Token:', refresh); // Debugging statement
+    // console.log('Access Token:', access); // Debugging statement
+    // console.log('Refresh Token:', refresh); // Debugging statement
 
     return refresh ? <Outlet /> : <Navigate to='/user/login'/>;
 };
